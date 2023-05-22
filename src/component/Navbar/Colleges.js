341ee2f223658db1695dfb92d21be1d688dfa3f5
Ram Rajput <rsrajput735416@gmail.com>
2023-05-22 23:21:44 +0530

    akash updated

--- src/component/Navbar/Colleges.js
+++ src/component/Navbar/Colleges.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import CollegeItem from './CollegeItem';
 
 const Colleges = () => {
   return (
-    <div>
-       Colleges
+    <div className="container py-5">
+      <div className="row pb-5 mb-4">
+        <div className="col-lg-3 col-md-6 mb-5">
+          <CollegeItem name='IIT Guwahati' courses={['Maths','Physics','Chemistry']} />
+        </div>
+      </div>
     </div>
   )
 }
